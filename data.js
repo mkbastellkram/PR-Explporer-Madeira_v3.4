b2a80097e8a8781193e732e05f1836418194c45e
mkbastellkram <mk.madeira2026@outlook.com>
2026-06-06 16:37:45 +0200

    PREv3.4.5b
    1. Kategorie „Tunnel“ ergänzt
       categoryId: tunnel
       baseIconId: tunnel
    
    2. Wasserfälle als Feature-Layer behandelt
       categoryId: waterfall
       baseIconId: waterfall
    
    3. Detailkarten-Zähler ergänzt
       Tunnel ≤100 m
       Wasserfälle ≤100 m
    
    4. Distanzprüfung gegen GPX-/KML-Geometrie vorbereitet
       Korridor: 100 m
    
    5. PR6.6 Túnel do Cavalo als Tunnel-Seed ergänzt
    
    6. PR16 Fajã do Rodrigues als Tunnel-/Wasserfall-Merkmal ergänzt
       mit mittlerem Vertrauensgrad, weil genaue OSM-Abschnitte später noch geprüft werden müssen
    
    7. Keine frei erfundenen OSM-Marker
       Einträge sind als kuratierte Seed-Daten oder verifizierungsbedürftige Routenmerkmale gekennzeichnet
    
    8. POI-Piktogramm „Tunnel“ ergänzt

--- data.js
+++ data.js
@@ -1,1 +1,15 @@
-<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Komoot</title><path d="M9.8 14.829l2.2-3.43 2.2 3.43 5.962 5.962A11.946 11.946 0 0 1 12 24c-3.043 0-5.935-1.14-8.162-3.209zM0 12C0 5.385 5.385 0 12 0c6.62 0 12 5.385 12 12 0 2.663-.855 5.175-2.469 7.284l-6.018-6.018c.15-.412.226-.839.226-1.27A3.743 3.743 0 0 0 12 8.257a3.743 3.743 0 0 0-3.739 3.739c0 .431.075.858.226 1.27l-6.018 6.018A11.865 11.865 0 0 1 0 12Z"/></svg>
+PR-Explorer V3.4.5B SAFE UPLOAD
+
+Diese ZIP ist eine reine Austauschversion.
+
+Vor dem Upload prüfen:
+- index.html muss mit <!doctype html> beginnen.
+- app.js muss mit (() => beginnen.
+- data.js muss mit window.PRX_DATA beginnen.
+- style.css muss CSS enthalten.
+
+Wenn Safari JavaScript-Code als Text zeigt, ist auf GitHub index.html falsch überschrieben worden.
+Dann nur index.html aus dieser ZIP erneut hochladen/ersetzen.
+
+Test-URL nach Upload:
+https://mkbastellkram.github.io/PR-Explorer-Madeira_v3.4/index.html?v=3.4.5b
