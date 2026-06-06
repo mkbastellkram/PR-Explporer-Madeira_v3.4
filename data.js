f67e87348aa659d71d0163e2bf6650760b5f2859
mkbastellkram <mk.madeira2026@outlook.com>
2026-06-06 17:05:10 +0200

    Delete data.js

--- data.js
+++
@@ -1,5 +0,0 @@
-ROOT-UPLOAD-KONTROLLE V3.4.5C
-Diese ZIP enthält Dateien direkt im ZIP-Root, keinen Oberordner.
-index.html muss auf GitHub direkt im Ordner PR-Explorer-Madeira_v3.4 liegen.
-index.html beginnt mit <!doctype html>
-app.js beginnt mit (() =>
